e33ca44aa87b2992654d27fcab972872a96f628c
Anant-p-sol <adwayaanant@gmail.com>
2025-03-23 12:30:21 +0530

    using javascript

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-let inputElement = document.createElement("input");
-inputElement.id = "myCheckbox";
-inputElement.type = "checkbox";
-document.body.appendChild(inputElement);
+let todoItemsContainer = document.getElementById("todoItemsContainer");
+let todoList = [
+    {
+        text: "Learn HTML"
+    },
+    {
+        text: "learn CSS"
+    },
+    {
+        text: "learn Javascript"
+    },
+    {
+        text: "learn Bootstrap"
+    }
+]
 
-let labelElement = document.createElement("label");
-labelElement.setAttribute("for","myCheckbox");
-labelElement.textContent = "Graduated";
-document.body.appendChild(labelElement);
+function createAndAppendTodo(todo) {
+    let todoElement = document.createElement("li");
+    todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoItemsContainer.appendChild(todoElement);
+
+    let inputElement = document.createElement("input");
+    inputElement.type = "checkbox";
+    inputElement.id = "checkboxInput";
+    inputElement.classList.add("checkbox-input");
+    todoElement.appendChild(inputElement);
+
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("d-flex", "flex-row", "label-container");
+    todoElement.appendChild(labelContainer);
+
+    let labelElement = document.createElement("label");
+    labelElement.setAttribute("for", "CheckboxInput");
+    todoElement.classList.add("checkbox-label");
+    labelElement.textContent = todo.text;
+    labelContainer.appendChild(labelElement);
+
+    let deleteIconContainer = document.createElement("div");
+    deleteIconContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteIconContainer);
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+    deleteIconContainer.appendChild(deleteIcon);
+}
+
+for(let Todo of todoList){
+    createAndAppendTodo(Todo);
+}
